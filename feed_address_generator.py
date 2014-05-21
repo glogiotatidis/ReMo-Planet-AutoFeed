f19017d4d4118b7cc5605529591c35536ddda306
Giorgos Logiotatidis <seadog@sealabs.net>
2014-05-21 18:52:00 +0300

    Add official reps blog.

--- feed_address_generator.py
+++ feed_address_generator.py
@@ -4,7 +4,9 @@ import requests
 BASE_URL = 'https://reps.mozilla.org'
 URL = '/api/v1/rep/?format=json&limit=20'
 
+
 def main():
+    entries = []
     new_url = URL
     while True:
         response = requests.get(BASE_URL + new_url, verify=False)
@@ -15,14 +17,28 @@ def main():
         for item in data['objects']:
             feed = item['profile']['personal_blog_feed'].strip()
             if feed:
-                print '[%s]' % feed
-                print 'name = %s' % item['fullname']
-                print 'avatar = %s' % item['profile']['avatar_url']
-                print ''
+                entries.append({
+                    'feed': feed,
+                    'name': item['fullname'],
+                    'avatar': item['profile']['avatar_url']
+                    })
 
         new_url = data['meta'].get('next', None)
         if not new_url:
             break
+
+    # Hardcoded official reps blog
+    entries.append({
+        'feed': 'https://blog.mozilla.org/mozillareps/feed/',
+        'name': 'Official Reps Blog',
+        'avatar': 'https://reps.mozilla.org/static/base/img/remo/remo_logo_medium.png',
+    })
+
+    for entry in entries:
+        print '[%s]' % entry['feed']
+        print 'name = %s' % entry['name']
+        print 'avatar = %s' % entry['avatar']
+        print ''
 
 
 if __name__ == '__main__':
